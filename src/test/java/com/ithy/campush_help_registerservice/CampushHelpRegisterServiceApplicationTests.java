6abafc8faf9a8125f4725a4d9d3a1066a3e0b977
huzhongbin <18702327006@163.com>
2021-05-28 19:47:46 +0800

    Initial commit

---
+++ src/test/java/com/ithy/campush_help_registerservice/CampushHelpRegisterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ithy.campush_help_registerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampushHelpRegisterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
